35a42ed00a85cf5576fda45c667ba51e20e3efc2
HesamSe <hesam.seyfollahi@gmail.com>
2025-03-23 19:32:23 +0330

    fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import '../app/globals.css'
+import Loading from "@/components/loading";
+
 type WordCount = {
     word: string;
     count: number;
@@ -7,6 +10,7 @@ type WordCount = {
 export default function Home() {
     const [words, setWords] = useState<WordCount[]>();
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
 
     const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -21,6 +25,7 @@ export default function Home() {
         formData.append('file', file);
 
         try {
+            setLoading(true)
             const response = await fetch('/api/upload', {
                 method: 'POST',
                 body: formData,
@@ -35,56 +40,66 @@ export default function Home() {
             }
         } catch {
             setError('Error uploading file');
+        } finally {
+            setLoading(false)
         }
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-            <div className="bg-white shadow-lg rounded-lg p-6 max-w-md w-full text-center">
-                <h1 className="text-xl font-semibold text-gray-700 mb-4">Upload a TXT File</h1>
-
-                <input
-                    type="file"
-                    accept=".pdf"
-                    onChange={handleUpload}
-                    className="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-600 file:text-white hover:file:bg-blue-700"
-                />
-
-                {words !== null && (
-                    <div className="mt-4">
-                        <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
-                            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-                                <tr>
-                                    <th scope="col" className="px-6 py-3">
-                                        Word
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                        Count
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {words?.map(wc => (
-                                <tr
-                                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                                    key={wc.word}
-                                >
-                                    <th scope="row"
-                                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {wc.word}
-                                    </th>
-                                    <td className="px-6 py-4">
-                                        {wc.count}
-                                    </td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-
-                {error && <p className="mt-4 text-red-500">{error}</p>}
+        <div className="flex flex-col gap-2 items-center h-screen w-screen bg-white dark:bg-gray-900 p-4">
+            <div className="flex-none flex flex-col items-center justify-center max-w-md min-h-36 min-w-96 text-center block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 text-gray-900 dark:text-white">
+                <h1 className="text-xl font-semibold mb-4">
+                    Upload a PDF File
+                </h1>
+                <div>
+                    {loading ? <Loading/> :
+                        <input
+                            type="file"
+                            accept=".pdf"
+                            onChange={handleUpload}
+                            className="block w-full text-sm file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-600 file:text-white hover:file:bg-blue-700"
+                        />
+                    }
+                </div>
             </div>
+            {!!words && (
+                <div className='flex-1 overflow-auto w-full'>
+                    <table className=' w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                        <thead
+                            className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Word
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Count
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {words?.map(wc => (
+                            <tr
+                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 max-w-full overflow-hidden"
+                                key={wc.word}
+                            >
+                                <th scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div className='max-w-72 truncate'>
+                                        {wc.word}
+                                    </div>
+                                </th>
+                                <td className="px-6 py-4">
+                                    {wc.count}
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+
+            {error && <p className="mt-4 text-red-500">{error}</p>}
+
         </div>
     );
 }
